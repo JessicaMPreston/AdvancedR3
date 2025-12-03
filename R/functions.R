39ccc3a7c6faeaf907d466e426042fa0b63661db
Jessica Minerva Preston <jessica.preston@sund.ku.dk>
2025-12-03 10:08:05 +0100

    remove function from learning document

--- R/functions.R
+++ R/functions.R
@@ -1,7 +1,6 @@
-
-#' create_table_descriptive_stats
+#' Calculate and make table of descriptive stats (mean & SD)
 #'
-#' @param df
+#' @param data = lipidomics
 #'
 #' @returns df/tibble
 
@@ -9,7 +8,7 @@ create_table_descriptive_stats <- function(data) {
   data |>
     dplyr::group_by(metabolite) |>
     dplyr::summarize(dplyr::across(value, list(mean = mean, sd = sd))) |>
-    dplyr::mutate(dplyr::across(where(is.numeric), \(x) round(x, digits = 1))) |>
+    dplyr::mutate(dplyr::across(tidyselect::where(is.numeric), \(x) round(x, digits = 1))) |>
     dplyr::mutate(MeanSD = glue::glue("{value_mean} ({value_sd})")) |>
     dplyr::select(Metabolite = metabolite, "Mean SD" = MeanSD)
 }
